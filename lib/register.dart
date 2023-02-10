b257cb7f91588af279ce6c4b81d043f3d3bee735
chandrashekhar2002 <shekhargandhala@gmail.com>
2023-02-10 13:04:29 +0530

    register changed

--- lib/register.dart
+++ lib/register.dart
@@ -1,105 +1,131 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'clipper.dart';
 import 'Auth.dart';
 import 'login.dart';
+import 'home.dart';
 
-class register extends StatelessWidget{
+class register extends StatelessWidget {
   var authentication = new Auth();
+  TextEditingController username = TextEditingController();
+  TextEditingController mobileNumber = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child:
-    Scaffold(
-        body: Stack(
-          children: [
-            ClipPath(
-              clipper: clipper(),
-              child: Container(
-                color: Colors.black87.withOpacity(0.7),
-              ),
-            ),
-            Positioned(
-              top: MediaQuery
-                  .of(context)
-                  .size
-                  .height / 3.15,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-
-                    Padding(padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 50,
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width / 1.15,
-                        child: TextField(
-                          controller: email,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Email',
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Padding(padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 50,
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width / 1.15,
-                        child: TextField(
-                          controller: password,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Padding(padding: EdgeInsets.all(20),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          authentication.handleSignUp(email.text, password.text).
-                            then((User? user) async{
-                              print("hello");
-                              if(!(user!.emailVerified)){
-                                user.sendEmailVerification();
-                                const snackBar = SnackBar(
-                                    content: Text('Please check your email to verify your email address')
-                                );
-                                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                              }
-                          });
-                        },
-                        child: Text("Register"),
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black87.withOpacity(0.7),
-                        ),
-                      ),),
-
-                    Padding(padding: EdgeInsets.all(20),
-                      child: TextButton(
-                          child: Text("Already have an acoount? SignIn"),
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => login()));
-                          }),
-                    ),
-                  ],
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(
+            children: [
+              ClipPath(
+                clipper: clipper(),
+                child: Container(
+                  color: Colors.black87.withOpacity(0.7),
                 ),
               ),
-            ),
-          ],
-        )
-    ),
+              Positioned(
+                top: MediaQuery.of(context).size.height / 3.15,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 1.15,
+                          child: TextField(
+                            controller: username,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Username',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 1.15,
+                          child: TextField(
+                            controller: mobileNumber,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Mobile Number',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 1.15,
+                          child: TextField(
+                            controller: email,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 1.15,
+                          child: TextField(
+                            controller: password,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            authentication
+                                .handleSignUp(email.text, password.text)
+                                .then((User? user) async {
+                              print("hello");
+                              if (!(user!.emailVerified)) {
+                                user.sendEmailVerification();
+                                const snackBar = SnackBar(
+                                    content: Text(
+                                        'Please check your email to verify your email address'));
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              }
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black87.withOpacity(0.7),
+                          ),
+                          child: const Text("Register"),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: TextButton(
+                            child: const Text("Already have an acoount? SignIn"),
+                            onPressed: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) => login()));
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
